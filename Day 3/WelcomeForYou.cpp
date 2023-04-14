96ca8bc9c705a8fc5236df552843ab080d4be7bc
ashish <ashish9six@gmail.com>
2023-04-14 08:11:31 +0530

    Day 3 Problem 1 Code

---
+++ Day 3/WelcomeForYou.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int num1, num2;
+    cin >> num1 >> num2;
+
+    if (num1 >= num2 )
+    {
+      cout << "Yes";
+    }
+    else cout << "No";
+    
+    return 0;
+}
